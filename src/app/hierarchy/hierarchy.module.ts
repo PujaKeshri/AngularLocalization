bc24140e35d71f7fd920417b398c20c0a496290e
Puja Keshri <puja.keshri@intellectdesign.com>
2018-10-31 11:14:23 +0530

    adde hierarchy module

---
+++ src/app/hierarchy/hierarchy.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HierarchyRoutingModule } from './hierarchy-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HierarchyRoutingModule
+  ],
+  declarations: []
+})
+export class HierarchyModule { }
